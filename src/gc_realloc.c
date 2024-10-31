5ca83450d019cdc2673c18915d2bdbb440184445
jeportie <jeportie@student.42.fr>
2024-10-31 08:25:30 +0000

    push pause

--- src/gc_realloc.c
+++ src/gc_realloc.c
@@ -6,18 +6,45 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 08:55:40 by jeportie          #+#    #+#             */
-/*   Updated: 2024/10/25 13:09:19 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/10/31 08:23:36 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libgc.h"
 
+static void	*helper(t_gc_node *current, void *ptr, size_t size, t_gc *gcl)
+{
+	size_t		old_size;
+	size_t		copy_size;
+	void		*new_ptr;
+
+	if (current->ptr == ptr)
+	{
+		old_size = current->size;
+		new_ptr = malloc(size);
+		if (!new_ptr)
+		{
+			write(2, "Error: gc_realloc malloc failed.\n", 33);
+			gc_cleanup(gcl);
+			exit(EXIT_FAILURE);
+		}
+		if (old_size < size)
+			copy_size = old_size;
+		else
+			copy_size = size;
+		gc_memcpy(new_ptr, ptr, copy_size);
+		free(current->ptr);
+		current->ptr = new_ptr;
+		current->size = size;
+		return (new_ptr);
+	}
+	return (NULL);
+}
+
 void	*gc_realloc(void *ptr, size_t size, t_gc *gcl)
 {
 	t_gc_node	*current;
 	void		*new_ptr;
-	size_t		old_size;
-	size_t		copy_size;
 
 	if (!ptr)
 		return (gc_malloc(size, gcl));
@@ -29,27 +56,11 @@ void	*gc_realloc(void *ptr, size_t size, t_gc *gcl)
 	current = gcl->head;
 	while (current)
 	{
-		if (current->ptr == ptr)
-		{
-			old_size = current->size;
-			new_ptr = malloc(size);
-			if (!new_ptr)
-			{
-				write(2, "Error: gc_realloc malloc failed.\n", 33);
-				gc_cleanup(gcl);
-				exit(EXIT_FAILURE);
-			}
-			if (old_size < size)
-				copy_size = old_size;
-			else
-				copy_size = size;
-			gc_memcpy(new_ptr, ptr, copy_size);
-			free(current->ptr);
-			current->ptr = new_ptr;
-			current->size = size;
+		new_ptr = helper(current, ptr, size, gcl);
+		if (!new_ptr)
+			current = current->next;
+		else
 			return (new_ptr);
-		}
-		current = current->next;
 	}
 	write(2, "Error: Pointer not managed by GC.\n", 34);
 	gc_cleanup(gcl);
