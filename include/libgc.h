48a3df014dd08195d97193d1ea0c9e92835c39e6
jeportie <jeportie@student.42.fr>
2024-12-13 17:12:51 +0000

    push pause

--- include/libgc.h
+++ include/libgc.h
@@ -6,7 +6,7 @@
 /*   By: jeportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 18:59:02 by jeportie          #+#    #+#             */
-/*   Updated: 2024/12/13 15:47:34 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/12/13 17:10:31 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,24 +55,31 @@ typedef struct s_garbage_collector
 }					t_gc;
 
 /* GARBAGE_COLLECTOR */
-void			*gc_init(void);
-void			*gc_malloc(size_t size, t_gc *gcl);
-void			*gc_realloc(void *ptr, size_t size, t_gc *gcl);
-void			gc_register(void *ptr, t_gc *gcl);
-void			gc_nest_register(void *ptr, t_gc *gcl);
-void			gc_fd_register(int fd, t_gc *gcl);
-void			gc_temp_file_register(const char *filename, t_gc *gcl);
-void			gc_cleanup(t_gc *gcl);
-void			gc_collect(t_gc *gcl);
-void			gc_free(void *ptr, t_gc *gcl);
-void			gc_lock(void *ptr, t_gc *gcl);
-void			gc_nest_lock(void *ptr, t_gc *gcl);
-void			gc_unlock(void *ptr, t_gc *gcl);
-void			gc_mark(void *ptr, t_gc *gcl);
+void	*gc_init(void);
+void	*gc_malloc(size_t size, t_gc *gcl);
+void	*gc_realloc(void *ptr, size_t size, t_gc *gcl);
+void	gc_register(void *ptr, t_gc *gcl);
+void	gc_nest_register(void *ptr, t_gc *gcl);
+void	gc_fd_register(int fd, t_gc *gcl);
+void	gc_temp_file_register(const char *filename, t_gc *gcl);
+void	gc_cleanup(t_gc *gcl);
+void	gc_collect(t_gc *gcl);
+void	gc_free(void *ptr, t_gc *gcl);
+void	gc_lock(void *ptr, t_gc *gcl);
+void	gc_nest_lock(void *ptr, t_gc *gcl);
+void	gc_unlock(void *ptr, t_gc *gcl);
+void	gc_mark(void *ptr, t_gc *gcl);
 
-void			*gc_memcpy(void *dest, const void *src, size_t n);
-char			*gc_strjoin(char *s1, char *s2, t_gc *gcl);
-int				gc_strlen(const char *str);
-char			*gc_strdup(const char *s, t_gc *gcl);
+/* ARENA_COLLECTOR */
+
+int		gc_init_arena(t_gc *gcl);
+void	*gc_arena_alloc(size_t size, int id, t_gc *gcl);
+void	gc_collect_arena(int id, t_gc *gcl);
+
+/* UTILS */
+void	*gc_memcpy(void *dest, const void *src, size_t n);
+char	*gc_strjoin(char *s1, char *s2, t_gc *gcl);
+int		gc_strlen(const char *str);
+char	*gc_strdup(const char *s, t_gc *gcl);
 
 #endif /*LIBGC_H*/
